157ff894d03eb83e08cbba03dce7abee51c70426
fan777 <fan777@gmail.com>
2021-10-01 19:56:47 -0700

    minor updates

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -5,12 +5,13 @@ import './VendingMachine.css';
 const VendingMachine = () => {
   return (
     <div className='VendingMachine'>
-      <div>
-        <img src='images/vending-machine-clipart.svg' alt='Vending Machine' />
-      </div>
-      <div>
-        <Link exact to='/cheetos'>Cheetos</Link> <Link exact to='/steak'>Steak</Link> <Link exact to='/gummy-bears'>Gummy Bears</Link>
-      </div>
+      <h2>What do you want?!?!?</h2>
+      <img src='images/vending-machine-clipart.svg' alt='Vending Machine' />
+      <ul>
+        <li><Link exact to='/cheetos'>Cheetos</Link> </li>
+        <li><Link exact to='/steak'>Steak</Link> </li>
+        <li> <Link exact to='/gummy-bears'>Gummy Bears</Link></li>
+      </ul>
     </div >
   );
 }
